b9ba3813d35dd46a15dcae3924b20e8ed7edc179
chrismontone <chris.montone@gmail.com>
2017-04-25 14:44:58 -0400

    React Weather App

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '5b7e5e10fa1a17c18b03d931eb4ed3f3';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEAHTER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
---
+++ src/components/chart.js
@@ -0,0 +1,22 @@
+/**
+ * Created by cmontone on 4/25/17.
+ */
+import _ from 'lodash';
+import React from 'react';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
+
+function average(data) {
+    return _.round(_.sum(data)/data.length);
+}
+
+export default (props) => {
+    return (
+        <div>
+            <Sparklines width={180} height={180} margin={0} data={props.data}>
+                <SparklinesLine color={props.color} />
+                <SparklinesReferenceLine type="avg" />
+            </Sparklines>
+            <div>{average(props.data)} {props.units}</div>
+        </div>
+    )
+}
